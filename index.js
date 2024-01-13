d7376de7214edf4ae6c1523dc7bf567e92ef783a
Protap Ghosh <protapg518@gmail.com>
2024-01-13 23:57:42 +0600

    create API for user admin role

--- index.js
+++ index.js
@@ -32,6 +32,11 @@ async function run() {
       const cartCollection = client.db("foodieDB").collection("carts");
 
       //  Users related API
+      app.get("/users", async (req, res) => {
+         const result = await usersCollection.find().toArray();
+         res.send(result);
+      });
+
       app.post("/users", async (req, res) => {
          const user = req.body;
          const query = { email: user.email };
@@ -41,6 +46,18 @@ async function run() {
          }
          const result = await usersCollection.insertOne(user);
          res.send(result);
+      });
+
+      app.patch("/users/admin/:id", async (req, res) => {
+         const id = req.params.id;
+         const filter = { _id: new ObjectId(id) };
+         const updateDoc = {
+            $set: {
+               role: "admin",
+            },
+        };
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
       });
 
       // Menu related API
