2824de5b211f60b57ed0e3c5dcdfb4ae9f6847e5
Protap Ghosh <protapg518@gmail.com>
2024-01-28 13:34:29 +0600

    create stripe payment system API

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // Middleware
@@ -199,6 +200,21 @@ async function run() {
          const query = { _id: new ObjectId(id) };
          const result = await cartCollection.deleteOne(query);
          res.send(result);
+      });
+
+      // Payment method intent
+      app.post("/create-payment-intent", async (req, res) => {
+         const { price } = req.body;
+         const amount = price * 100;
+         const paymentIntent = await stripe.paymentIntents.create({
+            amount: amount,
+            currency: "USD",
+            payment_method_type: ["card"],
+         });
+
+         res.send({
+            clientSecret: paymentIntent.client_secret,
+         });
       });
 
       // Send a ping to confirm a successful connection
